11dac2a119cbc81f5b4cdedecd038c41d2e87d55
HannahFreitas <hannahcassia@gmail.com>
2022-03-29 17:12:33 -0300

    correcting erros in components

--- src/main/kotlin/br/com/projetokotlin/mapper/UserFormMapper.kt
+++ src/main/kotlin/br/com/projetokotlin/mapper/UserFormMapper.kt
@@ -2,7 +2,9 @@ package br.com.projetokotlin.mapper
 
 import br.com.projetokotlin.dto.UserForm
 import br.com.projetokotlin.model.User
+import org.springframework.stereotype.Component
 
+@Component
 class UserFormMapper: Mapper<UserForm, User> {
 
     override fun map(t: UserForm): User {
